8611d077f1c7fc551d36f1fe3dbb277142ceb16b
buexplain <buexplain@163.com>
2019-10-27 22:24:33 +0800

    收工

--- app/http/controllers/backend/article/content/content.go
+++ app/http/controllers/backend/article/content/content.go
@@ -217,6 +217,35 @@ func DestroyBatch(ctx *fool.Ctx, w *fool.Response, r *fool.Request) error {
 		JSON(http.StatusOK)
 }
 
+//查看
+func Show(ctx *fool.Ctx, w *fool.Response, r *fool.Request) error {
+	//内容
+	result := new(m_content.Content)
+	result.ID = r.ParamInt("id", 0)
+	if result.ID <= 0 {
+		return w.JumpBack("参数错误")
+	}
+	if ok, err := dao.Dao.Get(result); err != nil {
+		return ctx.Error().WrapServer(err).Location()
+	} else if !ok {
+		return w.JumpBack("参数错误")
+	}
+
+	//内容的标签
+	contentTagList := make(m_contentTag.List, 0)
+	if err := dao.Dao.Where("ContentID=?", result.ID).Find(&contentTagList); err != nil {
+		return ctx.Error().WrapServer(err)
+	}
+
+	return w.
+		Assign("tagList", tagList).
+		Assign("result", result).
+		Assign("contentTagList", contentTagList).
+		Assign(boot.Config.CSRF.Field, csrf.TemplateField(r.Raw())).
+		Layout("backend/layout/layout.html").View(http.StatusOK, "backend/article/content/create.html")
+}
+
+
 //设置上下线
 func Online(ctx *fool.Ctx, w *fool.Response, r *fool.Request) error {
 	result := new(m_content.Content)
